f18db5b1b1d036a3bf213552f59e4c51fe05f304
Joe Gibson <gibsjose@mail.gvsu.edu>
2013-10-06 00:19:12 -0400

    Initial Commit

---
+++ TermProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TermProject
+//
+//  Created by Joe Gibson on 10/6/13.
+//  Copyright (c) 2013 Joe Gibson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
